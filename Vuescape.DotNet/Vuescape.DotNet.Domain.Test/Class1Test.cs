4a35b8726cfb60aaaa0c783836b67fcc7d431284
randylevy <randy.levy@gmail.com>
2021-03-17 14:52:23 -0400

    Add Class1 as test class

--- Vuescape.DotNet/Vuescape.DotNet.Domain.Test/Class1Test.cs
+++ Vuescape.DotNet/Vuescape.DotNet.Domain.Test/Class1Test.cs
@@ -9,6 +9,7 @@ namespace Vuescape.DotNet.Domain.Test
     using System;
 
     using OBeautifulCode.Assertion.Recipes;
+    using OBeautifulCode.CodeGen.ModelObject.Recipes;
 
     using Xunit;
 
@@ -17,6 +18,13 @@ namespace Vuescape.DotNet.Domain.Test
     /// </summary>
     public static partial class Class1Test
     {
+        static Class1Test()
+        {
+            // If the constructor cannot throw, clear all scenarios by calling ConstructorArgumentValidationTestScenarios.RemoveAllScenarios() and add ConstructorArgumentValidationTestScenario<Class1>.ConstructorCannotThrowScenario.
+            ConstructorArgumentValidationTestScenarios.RemoveAllScenarios();
+            ConstructorArgumentValidationTestScenarios.AddScenario(ConstructorArgumentValidationTestScenario<Class1>.ConstructorCannotThrowScenario);
+        }
+
         [Fact]
         public static void Method___Should_do_something___When_called()
         {
--- Vuescape.DotNet/Vuescape.DotNet.Domain/Class1.cs
+++ Vuescape.DotNet/Vuescape.DotNet.Domain/Class1.cs
@@ -6,10 +6,26 @@
 
 namespace Vuescape.DotNet.Domain
 {
+    using OBeautifulCode.Type;
+
     /// <summary>
     /// TODO: Starting point for new project.
     /// </summary>
-    public partial class Class1
+    public partial class Class1 : IModelViaCodeGen
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Class1"/> class.
+        /// </summary>
+        /// <param name="isCodeGen">This is the parameter.</param>
+        public Class1(
+            bool isCodeGen)
+        {
+            this.IsCodeGen = isCodeGen;
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the thingee is frobbed.
+        /// </summary>
+        public bool IsCodeGen { get; private set; }
     }
 }

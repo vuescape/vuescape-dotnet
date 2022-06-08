686d568db4a57d7bedbdb976b541ac29eda55b2c
randylevy <randy.levy@gmail.com>
2022-06-07 22:43:48 -0400

    Add DownloadFileOp

---
+++ Vuescape.DotNet/Vuescape.DotNet.Domain/Models/Feature/Ops/DownloadFileOp.cs
@@ -0,0 +1,35 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="DownloadFileOp.cs" company="Vuescape">
+//    Copyright (c) Vuescape 2021. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+// ReSharper disable once CheckNamespace
+namespace Vuescape.DotNet.Domain
+{
+    using OBeautifulCode.Assertion.Recipes;
+    using OBeautifulCode.DataStructure;
+    using OBeautifulCode.Type;
+
+    /// <summary>
+    /// An operation to download a file.
+    /// </summary>
+    public partial class DownloadFileOp : LinkedResourceOpBase, IModelViaCodeGen
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DownloadFileOp"/> class.
+        /// </summary>
+        /// <param name="linkedResource">The linked resource.</param>
+        public DownloadFileOp(
+            ILinkedResource linkedResource)
+            : base(linkedResource)
+        {
+            linkedResource.MustForArg(nameof(linkedResource)).NotBeNull();
+        }
+
+        /// <summary>
+        /// Gets the LinkTarget.
+        /// </summary>
+        public LinkTarget LinkTarget => LinkTarget.Download;
+    }
+}

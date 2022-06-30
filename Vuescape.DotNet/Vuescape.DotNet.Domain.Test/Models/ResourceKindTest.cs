b111a47e4f9dec0d9ca6380d4c2f7b27861fb4a6
randylevy <randy.levy@gmail.com>
2022-06-30 11:58:56 -0400

    Add ResourceKindTest to ensure Vuescape and OBC types are equivalent

---
+++ Vuescape.DotNet/Vuescape.DotNet.Domain.Test/Models/ResourceKindTest.cs
@@ -0,0 +1,38 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SlottedUiObjectTest.cs" company="Vuescape">
+//    Copyright (c) Vuescape 2021. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+// ReSharper disable once CheckNamespace
+namespace Vuescape.DotNet.Domain.Test
+{
+    using System;
+    using System.Linq;
+
+    using OBeautifulCode.Assertion.Recipes;
+    using OBeautifulCode.Enum.Recipes;
+    
+    using Xunit;
+
+    public class ResourceKindTest
+    {
+        [Fact]
+        public void ResourceKind___Should_have_same_names_and_values_as_OBC_ResourceKind___Always()
+        {
+            // TODO: When OBC has Bson* values then remove where
+            var vuescapeResourceKinds = typeof(ResourceKind)
+                .GetDefinedEnumValues()
+                .Where(_ =>
+                    (ResourceKind)Convert.ChangeType(_, typeof(ResourceKind)) != ResourceKind.BsonAsBytes &&
+                    (ResourceKind)Convert.ChangeType(_, typeof(ResourceKind)) != ResourceKind.BsonAsText)
+                .ToDictionary(_ => Enum.GetName(_.GetType(), _), _ => Convert.ChangeType(_, _.GetTypeCode()));
+
+            var obcResourceKinds = typeof(OBeautifulCode.DataStructure.UrlLinkedResourceKind)
+                .GetDefinedEnumValues()
+                .ToDictionary(_ => Enum.GetName(_.GetType(), _), _ => Convert.ChangeType(_, _.GetTypeCode()));
+
+            vuescapeResourceKinds.MustForTest(nameof(vuescapeResourceKinds)).BeEqualTo(obcResourceKinds);
+        }
+    }
+}

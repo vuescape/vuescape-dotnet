f7ce5502155ca8ff80c8eff8752cad8628436b98
randylevy <randy.ley@gmail.com>
2025-04-21 01:35:41 -0400

    Codegen

---
+++ Vuescape.DotNet/Vuescape.DotNet.Domain/Models/DynamicUI/PaneComponent/FileUploadComponent.cs
@@ -0,0 +1,31 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="FileUploadComponent.cs" company="Vuescape">
+//    Copyright (c) Vuescape 2021. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+// ReSharper disable once CheckNamespace
+namespace Vuescape.DotNet.Domain
+{
+    using OBeautifulCode.Type;
+
+    /// <summary>
+    /// Represents a File Upload component with an associated payload.
+    /// </summary>
+    public partial class FileUploadComponent : PaneComponent<FileUploadComponentPayload>, IModelViaCodeGen
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FileUploadComponent"/> class.
+        /// </summary>
+        /// <param name="payload">The payload containing details of the title component.</param>
+        public FileUploadComponent(FileUploadComponentPayload payload)
+            : base(payload)
+        {
+        }
+
+        /// <summary>
+        /// Gets the type of the pane component.
+        /// </summary>
+        public override string Type => "fileUpload";
+    }
+}
---
+++ Vuescape.DotNet/Vuescape.DotNet.Domain/Models/DynamicUI/PaneComponent/Payload/FileUploadComponentPayload.cs
@@ -0,0 +1,68 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="FileUploadComponentPayload.cs" company="Vuescape">
+//    Copyright (c) Vuescape 2021. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+// ReSharper disable once CheckNamespace
+namespace Vuescape.DotNet.Domain
+{
+    using System.Collections.Generic;
+    using OBeautifulCode.Assertion.Recipes;
+    using OBeautifulCode.Type;
+
+    /// <summary>
+    /// Represents the file upload component payload.
+    /// </summary>
+    public partial class FileUploadComponentPayload : IModelViaCodeGen
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FileUploadComponentPayload"/> class.
+        /// </summary>
+        /// <param name="id">The unique identifier for this component.</param>
+        /// <param name="title">OPTIONAL The title to display.</param>
+        /// <param name="uploadInstructionText">OPTIONAL upload instructional text to display inside file upload area.</param>
+        /// /// <param name="maxFileSizeInBytes">OPTIONAL maximum file size in bytes -- used for validation.</param>
+        /// <param name="acceptFileTypeExtensions">OPTIONAL list containing the acceptable file type extensions. e.g. ".csv".</param>
+        public FileUploadComponentPayload(
+            string id,
+            string title,
+            string uploadInstructionText = null,
+            long? maxFileSizeInBytes = null,
+            IReadOnlyList<string> acceptFileTypeExtensions = null)
+        {
+            new { id }.AsArg().Must().NotBeNullNorWhiteSpace();
+
+            this.Id = id;
+            this.Title = title;
+            this.UploadInstructionText = uploadInstructionText;
+            this.MaxFileSizeInBytes = maxFileSizeInBytes;
+            this.AcceptFileTypeExtensions = acceptFileTypeExtensions;
+        }
+
+        /// <summary>
+        /// Gets the ID.
+        /// </summary>
+        public string Id { get; private set; }
+
+        /// <summary>
+        /// Gets the title.
+        /// </summary>
+        public string Title { get; private set; }
+
+        /// <summary>
+        /// Gets the upload instruction text.
+        /// </summary>
+        public string UploadInstructionText { get; private set; }
+
+        /// <summary>
+        /// Gets the maximum file size in bytes.
+        /// </summary>
+        public long? MaxFileSizeInBytes { get; private set; }
+
+        /// <summary>
+        /// Gets the accept file type extensions.
+        /// </summary>
+        public IReadOnlyList<string> AcceptFileTypeExtensions { get; private set; }
+    }
+}

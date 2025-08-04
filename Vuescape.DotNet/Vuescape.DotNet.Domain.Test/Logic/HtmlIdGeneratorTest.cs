9efac1c5dde2a9ff9685824e374d645f13f99cb4
randylevy <randy.levy@gmail.com>
2025-08-04 14:43:11 -0400

    Implement custom Base36 encoding

---
+++ Vuescape.DotNet/Vuescape.DotNet.Domain.Test/Logic/HtmlIdGeneratorTest.cs
@@ -0,0 +1,131 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="HtmlIdGeneratorTest.cs" company="Vuescape">
+//    Copyright (c) Vuescape 2021. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+// ReSharper disable once CheckNamespace
+namespace Vuescape.DotNet.Domain.Test
+{
+    using System;
+    using System.Collections.Concurrent;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using System.Text.RegularExpressions;
+    using System.Threading.Tasks;
+    using Xunit;
+
+    public static class HtmlIdGeneratorTest
+    {
+        private static MethodInfo GetToBase36()
+        {
+            return typeof(HtmlIdGenerator)
+                .GetMethod("ToBase36", BindingFlags.NonPublic | BindingFlags.Static)
+                ?? throw new InvalidOperationException("Could not find ToBase36 method");
+        }
+
+        [Fact]
+        public static void ToBase36_WithZero_ReturnsZeroString()
+        {
+            var value = new object[] { 0UL };
+            var systemUnderTest = GetToBase36();
+
+            var actual = (string)systemUnderTest.Invoke(null, value);
+
+            Assert.Equal("0", actual);
+        }
+
+        [Fact]
+        public static void ToBase36_WithOne_ReturnsOneString()
+        {
+            var value = new object[] { 1UL };
+            var systemUnderTest = GetToBase36();
+
+            var actual = (string)systemUnderTest.Invoke(null, value);
+
+            Assert.Equal("1", actual);
+        }
+
+        [Fact]
+        public static void ToBase36_WithThirtyFive_ReturnsZ()
+        {
+            var value = new object[] { 35UL };
+            var systemUnderTest = GetToBase36();
+
+            var actual = (string)systemUnderTest.Invoke(null, value);
+
+            Assert.Equal("z", actual);
+        }
+
+        [Fact]
+        public static void ToBase36_WithThirtySix_Returns10()
+        {
+            var value = new object[] { 36UL };
+            var systemUnderTest = GetToBase36();
+
+            var actual = (string)systemUnderTest.Invoke(null, value);
+
+            Assert.Equal("10", actual);
+        }
+
+        [Fact]
+        public static void ToBase36_With1234567890_ReturnsExpected()
+        {
+            var value = new object[] { 1234567890UL };
+            var systemUnderTest = GetToBase36();
+
+            var actual = (string)systemUnderTest.Invoke(null, value);
+
+            Assert.Equal("kf12oi", actual);
+        }
+
+        [Fact]
+        public static void ToBase36_WithMax63BitValue_ReturnsExpected()
+        {
+            // 2^63-1 = 9223372036854775807
+            var value = new object[] { 9223372036854775807UL };
+            var systemUnderTest = GetToBase36();
+
+            var actual = (string)systemUnderTest.Invoke(null, value);
+
+            Assert.Equal("1y2p0ij32e8e7", actual);
+        }
+
+        [Fact]
+        public static void NewId_ProducesOnlyLowercaseLettersAndDigits_AndLengthBetween11And13()
+        {
+            var regex = new Regex("^[0-9a-z]{11,13}$", RegexOptions.Compiled);
+            for (int i = 0; i < 100; i++)
+            {
+                var id = HtmlIdGenerator.NewId();
+                Assert.Matches(regex, id);
+                Assert.InRange(id.Length, 11, 13);
+            }
+        }
+
+        [Fact]
+        public static void NewId_IsUnique_InSingleThreadedLoop()
+        {
+            var seen = new HashSet<string>();
+            for (int i = 0; i < 1000; i++)
+            {
+                var id = HtmlIdGenerator.NewId();
+                Assert.DoesNotContain(id, seen);
+                seen.Add(id);
+            }
+        }
+
+        [Fact]
+        public static void NewId_IsUnique_WhenCalledInParallel()
+        {
+            var bag = new ConcurrentBag<string>();
+            Parallel.For(0, 1000, _ =>
+            {
+                bag.Add(HtmlIdGenerator.NewId());
+            });
+
+            Assert.Equal(1000, bag.Distinct().Count());
+        }
+    }
+}
--- Vuescape.DotNet/Vuescape.DotNet.Domain/Logic/HtmlIdGenerator.cs
+++ Vuescape.DotNet/Vuescape.DotNet.Domain/Logic/HtmlIdGenerator.cs
@@ -48,7 +48,44 @@ namespace Vuescape.DotNet.Domain
             // Read 64 bits, mask off the high (sign) bit to yield a non-negative 63-bit value,
             // then encode in base-36.
             var raw = BitConverter.ToUInt64(buffer, 0) & long.MaxValue;
-            var result = Convert.ToString((long)raw, 36);
+            var result = ToBase36(raw);
+
+            return result;
+        }
+
+        /// <summary>
+        /// Converts the specified 64-bit unsigned integer to its lowercase base-36 string representation.
+        /// </summary>
+        /// <param name="value">The unsigned 64-bit integer to convert.</param>
+        /// <returns>A string containing the base-36 representation of <paramref name="value"/>, using digits '0'–'9' and letters 'a'–'z'.
+        /// If <paramref name="value"/> is zero, returns "0".
+        /// </returns>
+        private static string ToBase36(ulong value)
+        {
+            const string alphabet = "0123456789abcdefghijklmnopqrstuvwxyz";
+
+            string result;
+            if (value == 0)
+            {
+                result = "0";
+            }
+            else
+            {
+                // max length for 63 bits in base-36 is ceil(log₃₆(2⁶³)) ≈ 13 chars
+                var buffer = new char[13];
+                var pos = buffer.Length;
+
+                while (value > 0)
+                {
+                    var digit = (int)(value % 36);
+                    buffer[--pos] = alphabet[digit];
+                    value /= 36;
+                }
+
+                // slice off the unused leading chars
+                result = new string(buffer, pos, buffer.Length - pos);
+            }
+
             return result;
         }
     }
